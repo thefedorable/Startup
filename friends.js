b8f9b8e750ba5a6428121a5f8a703118ce3160db
thefedorable <tanner.hartwell421@gmail.com>
2023-03-10 20:48:59 -0700

    Finish JS

---
+++ friends.js
@@ -0,0 +1,33 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  
+    const tableBodyEl = document.querySelector('#scores');
+  
+    if (scores.length) {
+        for (const [i, score] of scores.entries()) {
+          const positionTdEl = document.createElement('td');
+          const nameTdEl = document.createElement('td');
+          const scoreTdEl = document.createElement('td');
+    
+          positionTdEl.textContent = i + 1;
+          nameTdEl.textContent = score.name;
+          scoreTdEl.textContent = score.score;
+    
+          const rowEl = document.createElement('tr');
+          rowEl.appendChild(positionTdEl);
+          rowEl.appendChild(nameTdEl);
+          rowEl.appendChild(scoreTdEl);
+    
+          tableBodyEl.appendChild(rowEl);
+        }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to start tracking</td></tr>';
+    }
+  }
+  
+  loadScores();
+  
--- home.js
+++ home.js
@@ -7,6 +7,62 @@ function save() {
   }
 }
 
+function getCount() {
+    const count = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    localStorage.setItem("count",count);
+    saveScore(count);
+}
+
+function getPlayerName() {
+    return localStorage.getItem('userName') ?? 'Mystery player';
+}
+
+function clean() {
+    localStorage.clear();
+}
+
+function saveScore(score) {
+    const userName = this.getPlayerName();
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+    scores = this.updateScores(userName, score, scores);
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+
+  function updateScores(userName, score, scores) {
+    const newScore = { name: userName, score: score};
+
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (newScore.name === prevScore.name) {
+        delete scores[i];
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+
+      else if (score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+
+    return scores;
+  }
+
 //for loading
 for(let i = 1; i <= boxes; i++){
   if(localStorage.length > 0){
