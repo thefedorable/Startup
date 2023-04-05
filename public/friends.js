281b4e59b61be797cccfa8ec9220a8060408079e
thefedorable <tanner.hartwell421@gmail.com>
2023-04-04 23:05:41 -0600

    trying to figure out how to get rid of duplicates

--- public/friends.js
+++ public/friends.js
@@ -18,27 +18,31 @@ async function loadScores() {
   displayScores(scores);
 }
 
-function loadScores() {
+function displayScores(scores) {
     const tableBodyEl = document.querySelector('#scores');
   
     if (scores.length) {
-        for (const [i, score] of scores.entries()) {
-          const positionTdEl = document.createElement('td');
+      for (const [i, score] of scores.entries()) {
+        const positionTdEl = document.createElement('td');
           const nameTdEl = document.createElement('td');
           const scoreTdEl = document.createElement('td');
-    
+      
           positionTdEl.textContent = i + 1;
           nameTdEl.textContent = score.name;
           scoreTdEl.textContent = score.score;
-    
+      
           const rowEl = document.createElement('tr');
           rowEl.appendChild(positionTdEl);
           rowEl.appendChild(nameTdEl);
           rowEl.appendChild(scoreTdEl);
-    
+      
           tableBodyEl.appendChild(rowEl);
-        }
-    } else {
+
+      }
+
+    } 
+    
+    else {
       tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to start tracking</td></tr>';
     }
   }
--- public/home.js
+++ public/home.js
@@ -21,47 +21,99 @@ function clean() {
     localStorage.clear();
 }
 
-function saveScore(score) {
-    const userName = this.getPlayerName();
-    let scores = [];
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-      scores = JSON.parse(scoresText);
-    }
-    scores = this.updateScores(userName, score, scores);
+function updateScore(score) {
+  const scoreEl = document.querySelector('#score');
+  scoreEl.textContent = score;
+}
 
+async function saveScore(score) {
+  const userName = this.getPlayerName();
+  const newScore = { name: userName, score: score};
+
+  try {
+    const response = await fetch('/api/score', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newScore),
+    });
+
+    // Store what the service gave us as the high scores
+    const scores = await response.json();
     localStorage.setItem('scores', JSON.stringify(scores));
+  } catch {
+    // If there was an error then just track scores locally
+    this.updateScoresLocal(newScore);
+  }
+}
+
+function updateScoresLocal(newScore) {
+  let scores = [];
+  const scoresText = localStorage.getItem('scores');
+  if (scoresText) {
+    scores = JSON.parse(scoresText);
   }
 
-  function updateScores(userName, score, scores) {
-    const newScore = { name: userName, score: score};
-
-    let found = false;
-    for (const [i, prevScore] of scores.entries()) {
-      if (newScore.name === prevScore.name) {
-        delete scores[i];
-        scores.splice(i, 0, newScore);
-        found = true;
-        break;
-      }
-
-      else if (score > prevScore.score) {
-        scores.splice(i, 0, newScore);
-        found = true;
-        break;
-      }
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
     }
-
-    if (!found) {
-      scores.push(newScore);
-    }
-
-    if (scores.length > 10) {
-      scores.length = 10;
-    }
-
-    return scores;
   }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  localStorage.setItem('scores', JSON.stringify(scores));
+}
+
+// function saveScore(score) {
+//     const userName = this.getPlayerName();
+//     let scores = [];
+//     const scoresText = localStorage.getItem('scores');
+//     if (scoresText) {
+//       scores = JSON.parse(scoresText);
+//     }
+//     scores = this.updateScores(userName, score, scores);
+
+//     localStorage.setItem('scores', JSON.stringify(scores));
+//   }
+
+//   function updateScores(userName, score, scores) {
+//     const newScore = { name: userName, score: score};
+
+//     let found = false;
+//     for (const [i, prevScore] of scores.entries()) {
+//       if (newScore.name === prevScore.name) {
+//         delete scores[i];
+//         scores.splice(i, 0, newScore);
+//         found = true;
+//         break;
+//       }
+
+//       else if (score > prevScore.score) {
+//         scores.splice(i, 0, newScore);
+//         found = true;
+//         break;
+//       }
+//     }
+
+//     if (!found) {
+//       scores.push(newScore);
+//     }
+
+//     if (scores.length > 10) {
+//       scores.length = 10;
+//     }
+
+//     return scores;
+//   }
 
 //for loading
 for(let i = 1; i <= boxes; i++){
