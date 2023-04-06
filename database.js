80100e85ca5b6e1d92cb57ef73770e9238224311
thefedorable <tanner.hartwell421@gmail.com>
2023-04-05 18:07:40 -0600

    get login functioning

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
- const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -11,20 +13,49 @@ if (!userName) {
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
+const userCollection = client.db('startup').collection('user');
 const scoreCollection = client.db('startup').collection('score');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
 
 function addScore(score) {
   scoreCollection.insertOne(score);
 }
 
 function getHighScores() {
-  const query = {score: {$gt: 0}};
+  const query = {};
   const options = {
-    sort: {score: -1},
+    sort: { score: -1 },
     limit: 10,
   };
   const cursor = scoreCollection.find(query, options);
   return cursor.toArray();
 }
 
-module.exports = {addScore, getHighScores};
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
+};
